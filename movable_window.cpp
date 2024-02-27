639a0c010e984cd0b5d1c2944e5d27199db85637
Anrivlev <warcraft1921@gmail.com>
2024-02-28 00:24:17 +0300

    not as easy as it seemed

--- movable_window.cpp
+++ movable_window.cpp
@@ -1,70 +1,17 @@
 #include <GL/freeglut_std.h>
-#include <GL/glext.h>
-#include <GL/glcorearb.h>
-#include <GLES3/gl32.h>
-#include <vector>
 
 const int WINDOW_HEIGHT = 600;
 const int WINDOW_WIDTH = 800;
 const int x = 100;
 const int y = 200;
 const char WINDOW_NAME[] = "Rotating cube";
-const GLclampf BACKGROUND_RED = 0.5f;
-const GLclampf BACKGROUND_GREEN = 0.5f;
-const GLclampf BACKGROUND_BLUE = 0.0f;
-const GLclampf BACKGROUND_ALPHA = 0.0f;
-
-GLuint VAO;
-GLuint VBO;
-
-std::vector<GLfloat> cube_vertex_data = {
-    -1.0f,
-    -1.0f,
-    -1.0f,
-    -1.0f,
-    -1.0f,
-    1.0f,
-    -1.0f,
-    1.0f,
-    -1.0f,
-    -1.0f,
-    1.0f,
-    1.0f,
-    1.0f,
-    -1.0f,
-    -1.0f,
-    1.0f,
-    -1.0f,
-    1.0f,
-    1.0f,
-    1.0f,
-    -1.0f,
-    1.0f,
-    1.0f,
-    1.0f,
-};
-std::vector<unsigned int> cube_index_data = {
-    1, 2, 3,
-    2, 3, 4,
-    3, 4, 8,
-    3, 7, 8,
-    5, 6, 7,
-    6, 7, 8,
-    1, 2, 5,
-    2, 5, 6,
-    2, 4, 8,
-    2, 6, 8,
-    1, 3, 5,
-    3, 5, 7};
+const GLfloat BACKGROUND_RED = 0.5f;
+const GLfloat BACKGROUND_GREEN = 0.5f;
+const GLfloat BACKGROUND_BLUE = 0.0f;
 
 void RenderCB()
 {
 
-    glDrawElements(GL_TRIANGLES, cube_index_data.size(), GL_UNSIGNED_INT, (void *)0);
-    
-    glEnableVertexAttribArray(0);
-    glBindBuffer
-    
     glutSwapBuffers();
 }
 
@@ -95,17 +42,6 @@ void SpecialCB(int key, int x, int y)
     }
 }
 
-void CreateVertexArrays()
-{
-    glGenBuffers(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, cube_vertex_data.size() * sizeof(GLfloat), &cube_vertex_data[0], GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube_index_data.size() * sizeof(unsigned int), &cube_index_data[0], GL_STATIC_DRAW);
-    glBindVertexArray(VAO);
-}
-
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
@@ -115,15 +51,13 @@ int main(int argc, char *argv[])
 
     glutCreateWindow(WINDOW_NAME);
     glutSetCursor(GLUT_CURSOR_CROSSHAIR);
+    glutSetColor(1, BACKGROUND_BLUE, BACKGROUND_GREEN, BACKGROUND_BLUE);
     glutPostRedisplay();
 
     glutDisplayFunc(RenderCB);
     glutKeyboardFunc(KeyboardCB);
     glutSpecialFunc(SpecialCB);
 
-    CreateVertexArrays();
-
-    glClearColor(BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_BLUE, BACKGROUND_ALPHA);
     glutMainLoop();
 
     return 0;
